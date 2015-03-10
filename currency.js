4519f9068a593ae1ddafbdf3f6c5bba97c508683
Flaise <Flaise.Saffron@gmail.com>
2015-03-10 14:13:13 -0500

    Rendering procedurally generated form options

---
+++ currency.js
@@ -0,0 +1,10 @@
+'use strict'
+
+exports.all = [
+    {code: 'usd', displayName: 'US Dollars', supportsAMEX: true},
+    {code: 'eur', displayName: 'Euros'},
+    {code: 'thb', displayName: 'Thai baht'},
+    {code: 'hkd', displayName: 'Hong Kong Dollars'},
+    {code: 'sgd', displayName: 'Singapore Dollars'},
+    {code: 'aud', displayName: 'Australian Dollars'}
+]
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,29 @@
 'use strict'
 
 var express = require('express')
+var currency = require('../currency')
 
 var router = express.Router()
 
+var expirationDates = []
+{
+    var expiration = new Date()
+    for(var i = 0; i < 12 * 6; i += 1) {
+        var monthZeroBased = expiration.getMonth()
+        var realMonth = monthZeroBased + 1
+        var year = expiration.getFullYear()
+        expirationDates.push({
+            code: realMonth + '/' + year,
+            displayName: realMonth + '/' + (year.toString().slice(-2))
+        })
+        
+        expiration.setMonth(monthZeroBased + 1)
+    }
+}
+
 router.get('/', function(req, res) {
-    res.render('index', {error: undefined})
+    res.render('index', {error: undefined, currencies: currency.all,
+                         expirationDates: expirationDates})
 })
 
 module.exports = router
