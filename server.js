6b6263893a0a192e8d8a7034548bce8bc8b8d218
Flaise <Flaise.Saffron@gmail.com>
2015-03-10 17:12:34 -0500

    Simple Jasmine/Zombie unit tests

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 'use strict'
 
 var express = require('express')
+var http = require('http')
 
 
 var app = express()
 app.set('view engine', 'jade')
 app.use('/', require('./routes/index'))
 
-app.listen(8080)
-console.log('Listening on port 8080...')
+var httpServer
+
+exports.run = function(port) {
+    if(httpServer)
+        throw new Error()
+    httpServer = http.createServer(app)
+    httpServer.listen(port)
+}
+
+exports.stop = function(callback) {
+    if(!httpServer)
+        throw new Error()
+    httpServer.close(callback)
+    httpServer = undefined
+}
+
+
+if(require.main === module) {
+    exports.run(8080)
+    console.log('Listening on port 8080...')
+}
--- spec/server-spec.js
+++ spec/server-spec.js
@@ -1,41 +1,68 @@
 'use strict'
 
 var phantom = require('phantom')
+var Browser = require('zombie')
+var server = require('../server')
 
+
+var port = 9001
+Browser.localhost('bunnies.com', port)
+
+var browser = new Browser()
 
 describe('server', function() {
-    var phantomInstance
-    var page
+    beforeEach(function() {
+        server.run(port)
+    })
+
+    afterEach(function(done) {
+        server.stop(done)
+    })
     
-    beforeEach(function(done) {
-        console.log(1)
-        phantom.create(function(ph) {
-            console.log(2)
-            phantomInstance = ph
-            ph.createPage(function(pg) {
-                console.log(3)
-                page = pg
-                page.onConsoleMessage(function(msg) { console.log(msg); });
-                done()
+    it("can't load route /asdf", function(done) {
+        browser.visit('/asdf')
+            .then(function() {
+                throw new Error()
             })
-        })
-    })
-
-    afterEach(function() {
-        phantomInstance.exit()
-    })
-
-    it('can open google.com with phantom-jasmine', function(done) {
-        console.log(4)
-        page.open("http://www.google.com", function(status) {
-            console.log("opened google? ", status)
-            page.evaluate(function () {
-                return document.title
-            }, function(result) {
-                console.log('Page title is ' + result)
-                done()
+            .error(function() {
+                expect(browser.statusCode).toBe(404)
             })
-        })
+            .finally(done)
     })
+    
+    it('can load route /', function(done) {
+        browser.visit('/')
+            .then(function() {
+                browser.assert.success()
+            })
+            .finally(done)
+    })
+    
+    function assertFormTag() {
+        var elements = browser.queryAll('form')
+        expect(elements.length).toBe(1)
+        expect(elements[0].getAttribute('method').toLowerCase()).toBe('post')
+        browser.assert.attribute('form', 'action', '.')
+    }
+
+    describe('form usage', function() {
+        beforeEach(function(done) {
+            browser.visit('/')
+                .then(function() {
+                    browser.assert.success()
+                    assertFormTag()
+                })
+                .finally(done)
+        })
+
+        afterEach(function() {
+            assertFormTag()
+        })
+        
+        it('has no error message', function() {
+            expect(browser.queryAll('.error').length).toBe(0)
+        })
+        
+    })
+    
 })
-
